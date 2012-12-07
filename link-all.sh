6adc3b1c8dc9342ed92d86ef39db3281730058cc
Mark Przepiora <mark.przepiora@gmail.com>
2012-12-07 15:11:52 -0700

    Added script to install dotfiles

---
+++ link-all.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+# Define files to copy
+FILES=".vim .vimrc .gitconfig .githelpers .tmux.conf"
+
+# Current directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# On OSX, make sure we use gnutar
+if [ "`which gnutar`" != "" ]; then
+  TAR="`which gnutar`"
+  echo using gnutar
+else
+  TAR="`which tar`"
+  echo using tar
+fi
+
+# Backup existing dotfiles
+mkdir -p ~/backups
+"$TAR" --ignore-failed-read -C ~/ -zcf ~/backups/dotfiles-`date +%Y-%m-%d--%T`.tar.gz $FILES
+
+# Delete existing dotfiles
+(cd ~ && rm -rf $FILES)
+
+# Link new dotfiles
+echo "$FILES" |
+tr ' ' "\n" |
+while read file; do
+  ln -s "$DIR"/"$file" ~/
+done
+
+
