4592a3a7bbe7676a93d5b17cf240e8adcec06db8
Mark Przepiora <mark.przepiora@gmail.com>
2017-12-04 14:31:21 -0700

    Remove comments and unused functions from link-all

--- link-all.sh
+++ link-all.sh
@@ -3,11 +3,6 @@
 set -eu
 set -o pipefail
 
-# Generate absolute path from relative path. Portable! Requires only  and
-# /Users/mark/dotfiles, unlike solutions that use readlink, etc.
-#
-# $1     : relative filename
-# output : absolute path
 abspath() {
     if [ -d "$1" ]; then
         (cd "$1"; pwd)
@@ -22,33 +17,10 @@ abspath() {
     fi
 }
 
-# $1 : (required) the executable required
 has_executable() {
     command -v "$1" >/dev/null 2>&1
 }
 
-# $1 : (required) the name of the executable
-# $2 : (optional) the error message to print if not present
-require_executable() {
-    ( set +ux; set -e
-
-      local name="$1"
-      local error_message="$2"
-
-      if [ -z "$error_message" ]; then
-          local error_message="ERROR: You do not have $name installed."
-      fi
-
-      if ! has_executable "$name"; then
-          echo "$error_message" >&2
-          exit 1
-      fi
-    )
-}
-
-# $1 : (required) the command to check if a package is installed
-# $2 : (required) the package name
-# $3 : (optional) the executable to test first
 has_package() {
     (   set +u
         local test_command="$1"
@@ -63,18 +35,10 @@ has_package() {
     )
 }
 
-# $1 : (required) the package name
-# $2 : (optional) the executable to test first
 has_homebrew_package() {
     has_package "brew list" "$1" "$2"
 }
 
-# Example usage:
-#
-#     ensure_homebrew_packages_installed "the_silver_searcher ag" "mysql mysql" "jq jq"
-#
-# Each first string is the package name, the second is the executable to test
-# first before running expensive  commands.
 ensure_homebrew_packages_installed() {
     local to_install=""
     for package_and_executable in "$@"; do
