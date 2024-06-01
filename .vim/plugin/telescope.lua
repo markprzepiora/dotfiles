26f6110bb7aceee32fdd3813a80ba7e9d3fa2acc
Mark Przepiora <mark.przepiora@gmail.com>
2024-05-31 20:07:07 -0600

    [neovim] Restore <leader>F to fuzzy find current dir, and add <leader>b to do a string search

--- .vim/plugin/telescope.lua
+++ .vim/plugin/telescope.lua
@@ -14,3 +14,24 @@ require('telescope').setup{
     },
   },
 }
+
+vim.keymap.set(
+  {'n'},
+  '<leader>f',
+  '<cmd>Telescope find_files<cr>',
+  { noremap = true }
+)
+
+vim.keymap.set(
+  {'n'},
+  '<leader>F',
+  ':lua require("telescope.builtin").find_files({cwd = require("telescope.utils").buffer_dir()})<cr>',
+  { noremap = true }
+)
+
+vim.keymap.set(
+  {'n', 'v'},
+  '<leader>b',
+  '<cmd>Telescope grep_string<cr>',
+  { noremap = true }
+)
--- .vim/plugin/telescope.vim
+++
@@ -1,3 +0,0 @@
-if has('nvim')
-  nnoremap <leader>f <cmd>Telescope find_files<cr>
-endif
